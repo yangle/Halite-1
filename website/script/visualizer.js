a52d8777a9ab65604310d7fbf090fcd80136e4c7
Yang-Le Wu <yangleuu@gmail.com>
2017-01-10 15:33:53 -0500

    reduce visualizer FPS

--- website/script/visualizer.js
+++ website/script/visualizer.js
@@ -542,7 +542,7 @@ function showGame(game, $container, maxWidth, maxHeight, showmovement, isminimal
         var idle = (Object.keys(pressed).length === 0) && !shouldplay;
         setTimeout(function() {
             requestAnimationFrame(animate);
-        }, 1000 / (idle ? 20.0 : 80.0));
+        }, 1000 / (idle ? 5.0 : 30.0));
     }
 }
 
